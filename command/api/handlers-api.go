3d125b3df9d4fa384a75d2999c013078b4206983
Nicolinho <ritsuko_0@protonmail.com>
2025-07-15 12:16:35 -0400

    upd

--- command/api/handlers-api.go
+++ command/api/handlers-api.go
@@ -1,1 +1,83 @@
 package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+)
+
+// info we're receiving from frontend
+type stripePayload struct {
+	Currency string `json:"currency"`
+	Amount   string `json:"amount"`
+}
+
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message,omitempty"`
+	Content string `json:"content,omitempty"`
+	ID      int    `json:"id,omitempty"`
+}
+
+func (app *application) GetPaymentIntent(w http.ResponseWriter, r *http.Request) {
+
+	var payload stripePayload
+
+	err := json.NewDecoder(r.Body).Decode(&payload)
+
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	amount, err := strconv.Atoi(payload.Amount)
+
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	card := cards.Card{
+		Secret:   app.config.stripe.secret,
+		Key:      app.config.stripe.key,
+		Currency: payload.Currency,
+	}
+
+	okay := true
+
+	payint, msg, err := card.Charge(payload.Currency, amount)
+
+	if err != nil {
+		okay = false
+	}
+
+	if okay {
+		out, err := json.MarshalIndent(pi, "", "  ")
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+
+	} else {
+		j := jsonResponse{
+			OK:      true,
+			Message: msg,
+			Content: "",
+		}
+
+		out, err := json.MarshalIndent(j, "", "  ")
+		if err != nil {
+			app.errorLog.Println(err)
+
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(out)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(out)
+	}
+
+}
--- command/api/routes-api.go
+++ command/api/routes-api.go
@@ -4,18 +4,21 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 
-	mux.Use(cors.Handler(cors.Oprtions{
-		AllowedOrigins:     []string{"http://*", "http://*"},
-		AllowedMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:     []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		AllowedCredentials: false,
-		MaxAge:             300,
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: false,
+		MaxAge:           300,
 	}))
+
+	mux.Get("/api/payment-intent", app.GetPaymentIntent)
 
 	return mux
 }
